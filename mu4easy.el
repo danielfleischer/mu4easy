3755936212cd8efa97a1964adbdaa0bd7e2708df
Daniel Fleischer <danflscr@gmail.com>
2021-08-06 16:54:03 +0300

    Starting to work on a package

---
+++ mu4easy.el
@@ -0,0 +1,30 @@
+;;; mu4easy.el --- mu4e easy configuration           -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Daniel Fleischer
+
+;; Author: Daniel Fleischer <danflscr@gmail.com>
+;; Keywords: mail, org
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+
+
+(provide 'mu4easy)
+;;; mu4easy.el ends here
